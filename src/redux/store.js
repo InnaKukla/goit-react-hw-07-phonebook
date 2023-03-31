b3bd508b626e44d08ac96a17dcbad80826f0c53a
Inna Kukla <0969125133@ukr.net>
2023-04-01 01:33:28 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 
-import { contactsReducer, filtersReducer } from './slice';
+import { contactsReducer } from './sliceContacts';
+import { filtersReducer } from './sliceFilter';
 
 export const store = configureStore({
   reducer: {
